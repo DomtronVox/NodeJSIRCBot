2dbebe34c66981f6e55e84ade93b18c19d198519
DomtronVox <domtron.vox@Gmail.com>
2016-07-07 23:24:20 -0400

    Tweak logging a bit more after test showed some bugs.

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ Bot = exports.Bot = function(plugins, debug){
     //load the module with the plugin and pass the plugin config to 
     //  its init function. not plugins need the bot object and a conf file.
     for (plugin in plugins){
-        this.log("core", "debug", "Loaded plugin: "+plugin)
+        this.log("core", "info", "Loaded plugin: "+plugin)
         //TODO:handle errors when the given plugin name does not correspond to a file.
         //TODO: Make sure each plugin only has one config file. i.e. a plugin is only initiated once.  
         require("./plugins/"+plugin).Plugin(this, plugins[plugin]);
@@ -30,6 +30,8 @@ Bot.prototype = EventEmitter.prototype
 
 //handle logging messages
 Bot.prototype.log = function(plugin, type, msg){ 
-    if(type=="debug" && this.DEBUG) console.log("["+plugin+"]", type+":", msg);
-    else console.log("["+plugin+"]", type+":", msg);
+    //only print debug messages if debug is true
+    if(type != "debug" || this.debug) {
+        console.log("["+plugin+"]", type+":", msg);
+    }
 };
